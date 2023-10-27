59b54735c6762f107a3ee4986a5531b1359d699a
Vansh Dubey <vanshdubey101963@gmail.com>
2023-10-28 01:10:53 +0530

    added three different login screens

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,76 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: TcLogin(),
     );
+  }
+}
+
+class GettingStarted extends StatefulWidget {
+  const GettingStarted({super.key});
+
+  @override
+  State<GettingStarted> createState() => _GettingStartedState();
+}
+
+class _GettingStartedState extends State<GettingStarted> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image:AssetImage("assets/backgroundimages/gettingstarted.png")
+            ),
+            ),
+        ), 
+    );    
+  }
+}
+
+class PassengerLogin extends StatefulWidget {
+  const PassengerLogin({super.key});
+
+  @override
+  State<PassengerLogin> createState() => _PassengerLoginState();
+}
+
+class _PassengerLoginState extends State<PassengerLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image:AssetImage("assets/backgroundimages/bluelogin.png")
+            ),
+            ),
+        ), 
+    );    
+  }
+}
+
+class TcLogin extends StatefulWidget {
+  const TcLogin({super.key});
+
+  @override
+  State<TcLogin> createState() => _TcLoginState();
+}
+
+class _TcLoginState extends State<TcLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image:AssetImage("assets/backgroundimages/yellowlogin.png")
+            ),
+            ),
+        ), 
+    );    
   }
 }
